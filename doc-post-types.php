f038f3d4baf231f5402ab18b74bff7df9a6d1200
m-e-h <info@martyhelmick.com>
2016-06-30 20:19:30 -0400

    add files

---
+++ doc-post-types.php
@@ -0,0 +1,125 @@
+<?php
+/*
+Plugin Name: DOC Post Types
+Plugin URI: https://github.com/DioceseOfCharlotte/doc-post-types
+Description: DOC Content Types for WordPress.
+Version: 0.5.0
+Author: Marty Helmick
+License: GNU General Public License v2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: doc-posts
+GitHub Plugin URI: https://github.com/DioceseOfCharlotte/doc-post-types
+*/
+
+	/**
+	 * Singleton class that sets up and initializes the plugin.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return void
+	 */
+final class Doc_Posts_Plugin {
+
+	/**
+	 * Directory path to the plugin folder.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @var    string
+	 */
+	public $dir_path = '';
+
+	/**
+	 * Directory URI to the plugin folder.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @var    string
+	 */
+	public $dir_uri = '';
+
+	/**
+	 * Returns the instance.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return object
+	 */
+	public static function get_instance() {
+
+		static $instance = null;
+
+		if ( is_null( $instance ) ) {
+			$instance = new self;
+			$instance->setup();
+			$instance->includes();
+			$instance->setup_actions();
+		}
+
+		return $instance;
+	}
+
+	/**
+	 * Constructor method.
+	 *
+	 * @since  1.0.0
+	 * @access private
+	 * @return void
+	 */
+	private function __construct() {}
+
+	/**
+	 * Initial plugin setup.
+	 *
+	 * @since  1.0.0
+	 * @access private
+	 * @return void
+	 */
+	private function setup() {
+		$this->dir_path = trailingslashit( plugin_dir_path( __FILE__ ) );
+		$this->dir_uri  = trailingslashit( plugin_dir_url( __FILE__ ) );
+	}
+
+	/**
+	 * Loads include and admin files for the plugin.
+	 *
+	 * @since  1.0.0
+	 * @access private
+	 * @return void
+	 */
+	private function includes() {
+		require_once $this->dir_path . 'lib/extended-cpts.php';
+		require_once $this->dir_path . 'lib/extended-taxos.php';
+		require_once $this->dir_path . 'includes/post-types.php';
+		require_once $this->dir_path . 'includes/taxonomies.php';
+		require_once $this->dir_path . 'includes/bb-metaboxes.php';
+	}
+
+	/**
+	 * Sets up initial actions.
+	 *
+	 * @since  1.0.0
+	 * @access private
+	 * @return void
+	 */
+	private function setup_actions() {
+		register_activation_hook( __FILE__, array( $this, 'activation' ) );
+	}
+
+	public function activation() {
+		flush_rewrite_rules();
+	}
+}
+
+/**
+ * Gets the instance of the main plugin class.
+ *
+ * @since  1.0.0
+ * @access public
+ * @return object
+ */
+function doc_posts_plugin() {
+	return Doc_Posts_Plugin::get_instance();
+}
+
+doc_posts_plugin();
---
+++ includes/taxonomies.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * Taxonomies.
+ *
+ * @package  RCDOC
+ */
+
+add_action( 'init', 'doc_register_taxonomies' );
+
+/**
+ * Post Groups.
+ */
+function doc_department_cpts() {
+   $cpts = array( 'archive_post','bishop', 'deacon', 'development', 'education', 'finance', 'human_resources', 'hispanic_ministry', 'housing', 'info_tech', 'liturgy', 'macs', 'multicultural', 'planning', 'property', 'tribunal', 'vocation' );
+   return $cpts;
+}
+
+function doc_place_cpts() {
+   $cpts = array(
+	   'department',
+	   'parish',
+	   'school',
+   );
+   return $cpts;
+}
+
+function doc_home_tiles() {
+   $cpts = array(
+	   'department',
+	   'cpt_archive',
+   );
+   return array_merge( $cpts, doc_department_cpts() );
+}
+
+/**
+ * Register taxonomies.
+ *
+ * @since  0.1.0
+ * @access public
+ */
+function doc_register_taxonomies() {
+	register_extended_taxonomy('school_system', 'school', array(
+		'meta_box' => 'radio',
+		'dashboard_glance' => true,
+
+		'capabilities' => array(
+			'manage_terms' => 'manage_options',
+			'edit_terms'   => 'manage_options',
+			'delete_terms' => 'manage_options',
+			'assign_terms' => 'manage_options',
+		),
+	) );
+
+	register_extended_taxonomy('vicariate', 'parish', array(
+		'meta_box' => 'radio',
+		'dashboard_glance' => true,
+
+		'capabilities' => array(
+			'manage_terms' => 'manage_options',
+			'edit_terms'   => 'manage_options',
+			'delete_terms' => 'manage_options',
+			'assign_terms' => 'manage_options',
+		),
+	) );
+
+	register_extended_taxonomy( 'agency', doc_home_tiles(),
+		array(
+			'meta_box' => 'radio',
+			'dashboard_glance' => true,
+		),
+		array(
+			'singular' => 'Agency',
+			'plural'   => 'Agencies',
+			'slug'     => 'agencies',
+		)
+	);
+
+	register_extended_taxonomy( 'statistics_type', 'statistics_report', array(
+		'meta_box' => 'radio',
+		'dashboard_glance' => true,
+
+		'capabilities' => array(
+			'manage_terms' => 'manage_options',
+			'edit_terms'   => 'manage_options',
+			'delete_terms' => 'manage_options',
+			'assign_terms' => 'edit_statistics_reports',
+		),
+	) );
+
+	register_extended_taxonomy( 'filetype', 'document', array(
+
+		'meta_box' => 'radio',
+		'dashboard_glance' => true,
+
+		'capabilities' => array(
+			'manage_terms' => 'manage_options',
+			'edit_terms'   => 'manage_options',
+			'delete_terms' => 'manage_options',
+			'assign_terms' => 'edit_documents',
+		),
+	) );
+
+	register_extended_taxonomy( 'document_category', 'document', array(
+
+		'meta_box' => 'radio',
+		'dashboard_glance' => true,
+
+		'capabilities' => array(
+			'manage_terms' => 'manage_options',
+			'edit_terms'   => 'manage_options',
+			'delete_terms' => 'manage_options',
+			'assign_terms' => 'edit_documents',
+		),
+	) );
+
+	register_extended_taxonomy( 'document_type', 'document', array(
+
+		'meta_box' => 'radio',
+		'dashboard_glance' => true,
+
+		'capabilities' => array(
+			'manage_terms' => 'manage_options',
+			'edit_terms'   => 'manage_options',
+			'delete_terms' => 'manage_options',
+			'assign_terms' => 'edit_documents',
+		),
+	) );
+}
