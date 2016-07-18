d8a228d4224c51e363dcfcf85549a7496f7d8945
m-e-h <info@martyhelmick.com>
2016-07-18 09:39:29 -0400

    icon for geolocate

--- inc/bb-controls/class-control-address.php
+++ inc/bb-controls/class-control-address.php
@@ -46,9 +46,9 @@ class ButterBean_Control_Address extends ButterBean_Control {
 	public function get_template() {
 		?>
 
-		<div class="u-1of1 u-p1 u-1of2-md">
-			<input id="geocomplete" type="text" placeholder="Type in an address" value="" />
-			<input id="find" class="button" type="button" value="find" />
+		<div class="u-1of1 u-p1">
+			<input class="u-1of1 u-1of2-md" id="geocomplete" type="text" placeholder="Start typing an address" value="" />
+			<button id="find" class="button" type="button"/><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="v-icon"><path d="M12 8c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm8.94 3A8.994 8.994 0 0 0 13 3.06V1h-2v2.06A8.994 8.994 0 0 0 3.06 11H1v2h2.06A8.994 8.994 0 0 0 11 20.94V23h2v-2.06A8.994 8.994 0 0 0 20.94 13H23v-2h-2.06zM12 19c-3.87 0-7-3.13-7-7s3.13-7 7-7 7 3.13 7 7-3.13 7-7 7z"/></svg></button>
 		</div>
 		<div class="row">
 			<div class="u-1of1 u-p1">
