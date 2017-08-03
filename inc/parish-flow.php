73bc058b13e9e2db848f1f5cd150705974f222c8
m-e-h <info@martyhelmick.com>
2017-08-03 13:54:35 -0400

    Parish ID functions

--- inc/parish-flow.php
+++ inc/parish-flow.php
@@ -31,3 +31,45 @@ function doc_filter_gravityflow_webhook_args( $args, $entry, $current_step ) {
 	return $args;
 
 }
+
+
+// User Parish ID.
+function doc_user_parish_id_field( $user_contact_method ) {
+
+	if ( ! current_user_can( 'edit_users' ) ) {
+		return;
+	}
+
+	$user_contact_method['doc_user_parish_id'] = __( 'Parish ID', 'rcdoc' );
+
+	return $user_contact_method;
+
+}
+add_filter( 'user_contactmethods', 'doc_user_parish_id_field' );
+
+function get_users_parish_id() {
+	$user_id = get_current_user_id();
+	$users_parish_id = get_user_meta( $user_id, 'doc_user_parish_id', true );
+
+	return $users_parish_id;
+}
+
+function get_parish_id() {
+	$post_id = get_the_ID();
+	$parish_id = get_post_meta( $post_id, 'doc_parish_id', true );
+
+	return $parish_id;
+}
+
+function user_can_update_parish() {
+
+	if ( current_user_can( 'edit_parishs' ) ) {
+		return true;
+	}
+
+	if ( get_users_parish_id() !== get_parish_id() ) {
+		return false;
+	}
+
+	return current_user_can( 'parish_update_form' );
+}
